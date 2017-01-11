e0d41d1d90e3bcd78cabde1f12e08bc0165e6716
mickael <mickael@potion-of-wit.com>
2017-01-11 15:46:43 +0100

    init, load and get methods

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 'use strict';
-const fs = require('fs');
 const path = require('path');
+const request = require('request');
+const protocol = (window.location.href.split('/') || ['http:'])[0];
+const _ = require('lodash');
+let texts = {};
+let root = null;
 
-exports.init = function(localizedDirPath, localizedFilename) {
-    fs.readFile(path.join(localizedDirPath, localizedFilename + '.json'), 'utf8', function(err, contents) {
-        console.log(contents);
+exports.init = function (localizationRoot) {
+    root = localizationRoot.replace(/https?:\/\//, '');
+};
+
+exports.load = filename => new Promise((resolve, reject) => {
+    request(path.join(protocol + root, filename + '.json'), function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            texts = _.chain(body)
+                .map(entry => [entry.id, entry.value])
+                .fromPairs()
+                .value();
+            resolve(texts);
+        } else {
+            reject(response);
+        }
     });
+});
+
+exports.get = function (txtId) {
+    return texts[txtId];
 };
